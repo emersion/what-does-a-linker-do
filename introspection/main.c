b286f9aa52c820a3d025e5d3f23f570ae386a783
emersion <contact@emersion.fr>
2018-05-15 15:13:17 +0100

    introspection: add a malloc'ed pointer

--- introspection/main.c
+++ introspection/main.c
@@ -7,12 +7,16 @@ char foo[] = "hello world";
 char bar[32];
 
 int main() {
+	void *ptr = malloc(1);
+
 	printf("main                                         %10p\n", main);
 	printf("First address past program text (etext)      %10p\n", &etext);
 	printf("foo                                          %10p\n", foo);
 	printf("First address past initialized data (edata)  %10p\n", &edata);
 	printf("bar                                          %10p\n", bar);
 	printf("First address past uninitialized data (end)  %10p\n", &end);
+	printf("ptr                                          %10p\n", ptr);
 
+	free(ptr);
 	return EXIT_SUCCESS;
 }
